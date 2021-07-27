55fb6668ce4c228f8e280dbc60e7b65e1cabc11a
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2021-07-27 20:41:38 +0200

    Added buggy support for forking servers (draft commit)

--- refuzz/networkio/Channel.py
+++ refuzz/networkio/Channel.py
@@ -9,6 +9,7 @@ from   dataclasses import dataclass
 from   subprocess  import Popen
 from   ptrace.debugger import (PtraceDebugger,
                                PtraceProcess,
+                               ProcessEvent,
                                ProcessExit,
                                ProcessSignal,
                                NewProcessEvent,
@@ -29,6 +30,7 @@ SOCKET_SYSCALL_NAMES = SOCKET_SYSCALL_NAMES.union(('read', 'write'))
 class ChannelBase(ABC):
     def __init__(self, pobj: Popen, tx_callback: Callable, rx_callback: Callable,
             timescale: float):
+        debug("Setting up new ptrace-enabled channel")
         self._pobj = pobj
         self._tx_callback = tx_callback
         self._rx_callback = rx_callback
@@ -50,6 +52,7 @@ class ChannelBase(ABC):
         self._debugger.traceFork()
         self._proc = self._debugger.addProcess(self._pobj.pid, is_attached=True)
         self._proc.syscall_state.ignore_callback = self._ignore_callback
+        self._pending = []
 
     def _ignore_callback(self, syscall):
         return syscall.name not in self._syscall_whitelist
@@ -62,80 +65,146 @@ class ChannelBase(ABC):
                        break_on_entry: bool = False,
                        resume_process: bool = True,
                        **kwargs):
-        def prepare_process(process, ignore_callback):
+        def prepare_process(process, ignore_callback, syscall=True):
             if not process in self._debugger:
                 debugger.addProcess(process, is_attached=True)
             process.syscall_state.ignore_callback = ignore_callback
-            process.syscall()
+            if syscall:
+                process.syscall()
+
+        def process_syscall(process, syscall):
+            # ensure that the syscall has finished successfully before callback
+            if syscall and syscall.result != -1 and \
+                    (break_on_entry or syscall.result is not None):
+                syscall_callback(process, syscall, **kwargs)
+                debug(f"syscall processed: {syscall.format()}")
+            else:
+                debug(f"syscall request ignored")
+
+        def process_event(event, reset_syscall=False, intercept_syscall=True):
+            if event is None:
+                return
+            syscall_signum = signal.SIGTRAP
+            if self._debugger.use_sysgood:
+                syscall_signum |= 0x80
+            is_syscall = isinstance(event, ProcessSignal) and event.signum == syscall_signum
+
+            if not is_syscall:
+                try:
+                    raise event
+                except ProcessExit as event:
+                    exitcode = -256
+                    if event.exitcode is not None:
+                        exitcode = event.exitcode
+                    raise ChannelBrokenException(f"Process with {event.process.pid=} exited with code {exitcode}")
+                except ProcessSignal as event:
+                    debug(f"Target process with {event.process.pid=} received signal with {event.signum=}")
+                    event.display()
+                    signum = event.signum
+                    if signum == signal.SIGSTOP:
+                        signum = 0
+                    if reset_syscall:
+                        event.process.syscall(signum)
+                    else:
+                        event.process.cont(signum)
+                    exitcode = signal_to_exitcode(event.signum)
+                    return
+                except NewProcessEvent as event:
+                    # monitor child for syscalls as well. may be needed for multi-thread or multi-process targets
+                    debug(f"Target process forked, adding child process with {event.process.pid=} to debugger")
+                    prepare_process(event.process, ignore_callback, reset_syscall)
+                    if reset_syscall:
+                        event.process.parent.syscall()
+                    else:
+                        event.process.cont()
+                        event.process.parent.cont()
+                    return
+                except ProcessExecution as event:
+                    debug(f"Target process with {event.process.pid=} called exec; removing from debugger")
+                    self._debugger.deleteProcess(event.process)
+                    return
+            else:
+                # Process syscall enter or exit
+                debug(f"Target process with {event.process.pid=} requested a syscall")
+                state = event.process.syscall_state
+
+                sc = PtraceSyscall(event.process, self._syscall_options, event.process.getregs())
+                debug(f"Traced {sc.name=} with {state.name=} and {state.next_event=}")
+
+                syscall = state.event(self._syscall_options)
+
+                if intercept_syscall:
+                    process_syscall(event.process, syscall)
+
+                if reset_syscall:
+                    event.process.syscall()
+                else:
+                    event.process.cont()
+
+                return syscall
+
+        def stop_process(process):
+            if not process.is_stopped and not process.isTraced():
+                process.kill(signal.SIGSTOP)
+                # sending a signal would restart a syscall, so we clear state
+                process.syscall_state.clear()
+                debug(f"Sent SIGSTOP to interrupt target with {process.pid=}")
+                sigstopped = False
+                while not sigstopped:
+                    try:
+                        # discard received SIGSTOP signal, queue up any other event
+                        process.waitSignals(signal.SIGSTOP)
+                        sigstopped = True
+                    except ProcessEvent as event:
+                        syscall = process_event(event, reset_syscall=True, intercept_syscall=False)
+                        # if we receive a syscall, we add it to the queue
+                        if syscall:
+                            self._pending.append((event.process, syscall))
 
         with ThreadPoolExecutor() as executor:
-            for proc in self._debugger:
-                if not proc.is_stopped:
-                    proc.kill(signal.SIGSTOP)
-                    debug(f"Sent SIGSTOP to interrupt target with {proc.pid=}")
-                    while True:
-                        event = self._proc.waitEvent()
-                        if event.signum in (signal.SIGSTOP, signal.SIGTRAP):
-                            debug("Target received SIGSTOP/SIGTRAP and was interrupted")
-                            break
-
-                ## Break on next syscall
-                prepare_process(self._proc, ignore_callback)
+            for process in self._debugger:
+                prepare_process(process, ignore_callback, syscall=False)
+                stop_process(process)
+                process.syscall()
 
             ## Execute monitor target
             if monitor_target:
                 future = executor.submit(monitor_target)
 
             ## Listen for and process syscalls
+            shutdown = False
             while True:
                 if not self._debugger:
                     raise ChannelBrokenException("Process was terminated while waiting for syscalls")
 
+                if self._pending:
+                    process, syscall = self._pending.pop(0)
+                    process_syscall(process, syscall)
+                    continue
+
                 try:
-                    event = self._debugger.waitSyscall()
-                except ProcessExit as event:
-                    exitcode = -256
-                    if event.exitcode is not None:
-                        exitcode = event.exitcode
-                    raise ChannelBrokenException(f"Process with {event.processs.pid=} exited with code {exitcode}")
-                except ProcessSignal as event:
-                    debug(f"Target process with {event.process.pid=} received signal with {event.signum=}")
-                    event.display()
-                    event.process.syscall(event.signum)
-                    exitcode = signal_to_exitcode(event.signum)
-                    continue
-                except NewProcessEvent as event:
-                    # monitor child for syscalls as well. may be needed for multi-thread or multi-process targets
-                    debug(f"Target process forked, adding child process with {event.process.pid=} to debugger")
-                    prepare_process(event.process, ignore_callback)
-                    event.process.parent.syscall()
-                    continue
-                except ProcessExecution as event:
-                    debug(f"Target process with {event.process.pid=} called exec; removing from debugger")
-                    self._debugger.deleteProcess(event.process)
-                    continue
+                    event = self._debugger.waitSyscall(blocking=not shutdown)
+                    if event is not None:
+                        raise event
+                except ProcessEvent as e:
+                    event = process_event(e, reset_syscall=True, intercept_syscall=not shutdown)
+                    if event is None:
+                        continue
+                    elif shutdown:
+                        self._pending.append((event.process, event))
 
-                # Process syscall enter or exit
-                debug(f"Target proces with {event.process.pid=} requested a syscall")
-                state = event.process.syscall_state
-                syscall = state.event(self._syscall_options)
-                # ensure that the syscall has finished successfully before callback
-                if syscall and syscall.result != -1 and \
-                        (break_on_entry or syscall.result is not None):
-                    syscall_callback(event.process, syscall, **kwargs)
-                else:
-                    debug(f"syscall request ignored")
-
-                if break_callback():
-                    debug("Breaking out of debug loop")
+                if event is None:
                     break
 
-                # Break on next syscall
-                event.process.syscall()
+                if not shutdown:
+                    shutdown = break_callback()
+                    if shutdown:
+                        debug("Clearing remaining events and breaking out of debug loop")
 
-            ## Resume process and remove syscall breakpoints
+            ## Resume processes and remove syscall breakpoints
             if resume_process:
                 for process in self._debugger:
+                    stop_process(process)
                     process.cont()
                     debug(f"Resumed process with {process.pid=}")
 
@@ -143,9 +212,13 @@ class ChannelBase(ABC):
             if monitor_target:
                 return future.result()
 
+    def close(self):
+        # if self._debugger and self._proc.is_attached:
+        #     self._proc.terminate(wait_exit=True)
+        self._debugger.quit()
+
     def __del__(self):
-        if self._debugger and self._proc.is_attached:
-            self._proc.terminate(wait_exit=True)
+        self.close()
 
     @abstractmethod
     def send(self, data: ByteString) -> int:
@@ -231,14 +304,13 @@ class TCPChannel(ChannelBase):
             sockfd = syscall.result
 
         monitor_target = partial(self._socket.connect, address)
-        self._monitor_syscalls(monitor_target, ignore_callback, break_callback, syscall_callback_accept, listenfd=listenfd)
+        self._monitor_syscalls(monitor_target, ignore_callback, break_callback, syscall_callback_accept, resume_process=False, listenfd=listenfd)
         self._sockfd = sockfd
 
-    def send(self, data: ByteString) -> int:
-        sent = 0
-        while sent < len(data):
-            sent += self._send_sync(data[sent:])
+        # wait for the next read, recv, select, or poll, or wait for the parent to fork and trace child
+        self._poll_sync()
 
+    def _poll_sync(self):
         server_waiting = False
         # TODO add support for epoll?
         ignore_callback = lambda x: x.name not in ('read', 'recv', 'recvfrom', 'recvmsg', 'poll', 'ppoll', 'select')
@@ -271,11 +343,18 @@ class TCPChannel(ChannelBase):
             server_waiting = True
         self._monitor_syscalls(None, ignore_callback, break_callback, syscall_callback_read, break_on_entry=True)
 
+    def send(self, data: ByteString) -> int:
+        sent = 0
+        while sent < len(data):
+            sent += self._send_sync(data[sent:])
+
+        self._poll_sync()
         return sent
 
     def _send_sync(self, data: ByteString) -> int:
         server_received = 0
         client_sent = 0
+        ## Set up a barrier so that client_sent is ready when checking for break condition
         barrier = threading.Barrier(2)
 
         ignore_callback = lambda x: x.name not in ('read', 'recv', 'recvfrom', 'recvmsg')
@@ -283,6 +362,7 @@ class TCPChannel(ChannelBase):
             if not barrier.broken:
                 barrier.wait()
                 barrier.abort()
+            debug(f"{client_sent=}; {server_received=}")
             return client_sent > 0 and server_received >= client_sent
 
         def send_monitor(data):
@@ -318,15 +398,19 @@ class TCPChannel(ChannelBase):
             if self._socket not in poll:
                 return b''.join(chunks)
 
-            ret = self._socket.recv(RECV_CHUNK_SIZE)
+            ret = self._socket.recv(self.RECV_CHUNK_SIZE)
             if ret == b'' and len(chunks) == 0:
                 raise ChannelBrokenException("recv returned 0, socket shutdown")
 
             chunks.append(ret)
         return b''.join(chunks)
 
-    def __del__(self):
+    def close(self):
         self._socket.close()
+        super(TCPChannel, self).close()
+
+    def __del__(self):
+        self.close()
         super(TCPChannel, self).__del__()
 
 class UDPChannel(ChannelBase):
