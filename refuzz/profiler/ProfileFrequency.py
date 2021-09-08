553ad90e694164884733bbf564cfb300e399b37e
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2021-09-08 21:03:58 +0200

    Better frequency profiler

--- refuzz/profiler/ProfileFrequency.py
+++ refuzz/profiler/ProfileFrequency.py
@@ -1,26 +1,21 @@
 from profiler import PeriodicProfiler
-from collections import deque
-import datetime
 
 class ProfileFrequency(PeriodicProfiler):
-    def __init__(self, name, samples=10, **kwargs):
+    def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
-        self._samples = deque(maxlen=samples)
+        self._counter = 0
         self._frequency = None
 
     def __call__(self, obj):
         obj = super().__call__(obj)
         def func(*args, **kwargs):
-            self._samples.append(datetime.datetime.now())
+            self._counter += 1
             obj(*args, **kwargs)
         return func
 
     def _task(self):
-        if len(self._samples) >= 1:
-            duration = datetime.datetime.now() - self._samples[0]
-            self._frequency = len(self._samples) / duration.total_seconds()
-        else:
-            self._frequency = 0
+        self._frequency = self._counter / self._period
+        self._counter = 0
 
     @property
     def value(self):
