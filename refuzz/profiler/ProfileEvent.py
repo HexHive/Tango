07c1911fcf19fb341379b99af1fe6d85e5fc7725
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2021-09-09 15:05:43 +0200

    Augmented ProfileEvent with args and ret

--- refuzz/profiler/ProfileEvent.py
+++ refuzz/profiler/ProfileEvent.py
@@ -1,23 +1,40 @@
 from profiler import ProfilerBase
-from threading import Event
+from threading import Condition
 
 class ProfileEvent(ProfilerBase):
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
-        self._event = Event()
+        self._cv = Condition()
+        self._args = None
 
     def __call__(self, obj):
         def func(*args, **kwargs):
-            obj(*args, **kwargs)
-            self._event.set()
+            with self._cv:
+                ret = obj(*args, **kwargs)
+                self._args = (args, kwargs)
+                self._ret = ret
+                self._cv.notify_all()
         return func
 
     @property
     def value(self):
         return self
 
-    def __enter__(self):
-        self._event.wait()
+    @property
+    def args(self):
+        return self._args
 
-    def __exit__(self, type, value, traceback):
-        self._event.clear()
+    @property
+    def ret(self):
+        return self._ret
+
+    def __enter__(self):
+        self._cv.__enter__()
+        while self._args is None:
+            self._cv.wait()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self._args = None
+        self._ret = None
+        self._cv.__exit__(exc_type, exc_value, exc_traceback)
