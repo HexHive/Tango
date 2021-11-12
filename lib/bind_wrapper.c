e8bbc1b1cdc2b4ff202c21f668857546f93ee024
Qiang Liu <cyruscyliu@gmail.com>
2021-11-12 16:36:27 +0100

    Add bind wrapper: check and set SO_REUSEADDR
    1. declare __wrap_bind and __real_bind
    2. define __wrap_bind but don't define __real_bind
    3. add -Wl,-wrap,bind to LDFLAGS
    
    The linker replaces bind with __wrap_bind and link __real_bind to bind.

---
+++ lib/bind_wrapper.c
@@ -0,0 +1,41 @@
+/*
+ Copyright (c) 2021 Qiang Liu <cyruscyliu@gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/socket.h>
+
+int __wrap_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int __real_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+
+int __wrap_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    uint32_t optval;
+    uint32_t optlen;
+
+    /* get and set SO_REUSEADDR */
+    getsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &optval, &optlen);
+    if (optval != 0) {
+        printf("Socket: %d has enabled SO_BROADCAST\n", sockfd);
+    } else {
+        optval = 1;
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+        printf("Enable SO_BRODCAST on socket: %d\n", sockfd);
+    }
+
+    /* go to bind then */
+    return __real_bind(sockfd, addr, addrlen);
+}
