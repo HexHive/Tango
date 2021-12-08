b9b573515d187577aacfc47c9b5232d5eb6aa9a1
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2021-12-08 14:01:55 +0100

    Ignore callbacks must be preserved throughout a monitor session

--- refuzz/networkio/PtraceForkChannel.py
+++ refuzz/networkio/PtraceForkChannel.py
@@ -317,7 +317,7 @@ class PtraceForkChannel(ChannelBase):
         if self._proc_trapped:
             debug("Waking up forkserver :)")
             self._proc.syscall()
-            
+
             # must actually wait for syscall, not any event
             syscall_found = False
             ignore_callback = lambda x: False
@@ -325,7 +325,15 @@ class PtraceForkChannel(ChannelBase):
                 nonlocal syscall_found
                 syscall_found = True
             break_callback = lambda: syscall_found
+
+            # backup the old ignore_callbacks
+            for process in self._debugger:
+                process.syscall_state._ignore_callback = process.syscall_state.ignore_callback
             self._monitor_syscalls(None, ignore_callback, break_callback, syscall_callback, break_on_entry=True)
+            # restore the old ignore_callbacks
+            for process in self._debugger:
+                process.syscall_state.ignore_callback = process.syscall_state._ignore_callback
+                del process.syscall_state._ignore_callback
             self._proc_trapped = False
 
     def close(self, terminate=False):
