8443a449074cca632c40ce472787b1e7201b3871
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2022-01-07 16:40:03 +0100

    Fixed module import order

--- refuzz/fuzzer/__init__.py
+++ refuzz/fuzzer/__init__.py
@@ -3,5 +3,5 @@ logger = logging.getLogger("fuzzer")
 for func in ('debug', 'info', 'warning', 'error', 'critical'):
     setattr(sys.modules[__name__], func, getattr(logger, func))
 
-from .FuzzerSession import FuzzerSession
 from .FuzzerConfig  import FuzzerConfig
+from .FuzzerSession import FuzzerSession
