4ddac84c6f82507a832ca45b631c1bceb04df1d4
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2022-03-11 15:27:26 +0100

    Updated fuzzer default configuration

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from fuzzer import FuzzerSession, FuzzerConfig
 import logging
 
 logging.getLogger().setLevel(logging.WARN)
-logging.getLogger("statemanager").setLevel(logging.DEBUG)
+logging.getLogger("statemanager").setLevel(logging.INFO)
 logging.getLogger("networkio").setLevel(logging.INFO)
-logging.getLogger("fuzzer").setLevel(logging.DEBUG)
+logging.getLogger("fuzzer").setLevel(logging.CRITICAL)
+logging.getLogger("webui").setLevel(logging.INFO)
+logging.getLogger("ptrace").setLevel(logging.CRITICAL)
 
 config = FuzzerConfig("./targets/forkexec_server/fuzz.json")
 sess = FuzzerSession(config)
