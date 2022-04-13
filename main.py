ff6f356b1abb12a620c62c0940dde9bc7a3f3016
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2022-04-13 11:24:38 +0200

    Better logging

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import logging
 logging.getLogger().setLevel(logging.WARN)
 logging.getLogger("statemanager").setLevel(logging.INFO)
 logging.getLogger("networkio").setLevel(logging.INFO)
-logging.getLogger("fuzzer").setLevel(logging.CRITICAL)
+logging.getLogger("fuzzer").setLevel(logging.INFO)
 logging.getLogger("webui").setLevel(logging.INFO)
 logging.getLogger("ptrace").setLevel(logging.CRITICAL)
+logging.getLogger("input").setLevel(logging.INFO)
 
 config = FuzzerConfig("./targets/forkexec_server/fuzz.json")
 sess = FuzzerSession(config)
