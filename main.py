d9cc6d83724d3f1f2f48a3e96b141a1f02bb281e
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2022-10-13 14:56:45 +0200

    Cleaned up main.py

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 from tangofuzz import *
 
 from fuzzer import FuzzerSession, FuzzerConfig
+import argparse
 import logging
 
-logging.getLogger().setLevel(logging.WARN)
-logging.getLogger("statemanager").setLevel(logging.DEBUG)
-logging.getLogger("networkio").setLevel(logging.DEBUG)
-logging.getLogger("fuzzer").setLevel(logging.INFO)
-logging.getLogger("webui").setLevel(logging.INFO)
-logging.getLogger("ptrace").setLevel(logging.CRITICAL)
-logging.getLogger("input").setLevel(logging.INFO)
-logging.getLogger("generator").setLevel(logging.DEBUG)
-logging.getLogger("mutator").setLevel(logging.DEBUG)
-logging.getLogger("interaction").setLevel(logging.DEBUG)
-logging.getLogger("loader").setLevel(logging.DEBUG)
+def parse_args():
+    parser = argparse.ArgumentParser(description=(
+        "Launches a TangoFuzz fuzzing session."
+    ))
+    parser.add_argument("config",
+        help="The path to the TangoFuzz fuzz.json file.")
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+        help=("Controls the verbosity of messages. "
+            "-v prints info. -vv prints debug. Default: warnings and higher.")
+        )
+    return parser.parse_args()
 
-config = FuzzerConfig("./targets/zoom/fuzz.json")
-sess = FuzzerSession(config)
-sess.run()
+def configure_verbosity(level):
+    mapping = {
+        0: logging.WARNING,
+        1: logging.INFO,
+        2: logging.DEBUG
+    }
+    # will raise exception when level is invalid
+    numeric_level = mapping[level]
+    logging.getLogger().setLevel(numeric_level)
+
+def main():
+    args = parse_args()
+    configure_verbosity(args.verbose)
+
+    config = FuzzerConfig(args.config)
+    sess = FuzzerSession(config)
+    sess.run()
+
+if __name__ == '__main__':
+    main()
