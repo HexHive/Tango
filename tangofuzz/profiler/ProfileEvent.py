f5e1604efd765fa0e213e40d6b7001b9c655d5c5
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2023-01-18 20:32:45 +0100

    Improved EventProfiler's operability with asyncio loops

--- tangofuzz/profiler/ProfileEvent.py
+++ tangofuzz/profiler/ProfileEvent.py
@@ -14,11 +14,12 @@ class ProfileEvent(ProfilerBase):
         super().__init__(name, **kwargs)
         if not self._init_called:
             self._args = None
-            self._listeners = []
+            self._listeners = {}
 
     def _notify_listeners(self, argt, ret):
-        for loop in self._listeners:
-            run_coroutine_threadsafe(self._listener_notify(argt, ret), loop)
+        for loop, contexts in self._listeners.items():
+            for context in contexts:
+                run_coroutine_threadsafe(context.notify(argt, ret), loop)
 
     def ___call___(self, obj):
         def func(*args, **kwargs):
@@ -49,24 +50,48 @@ class ProfileEvent(ProfilerBase):
     def ret(self):
         return self._ret
 
+    def _create_event_context(self):
+        return EventContextManager(self)
+
+    async def _listener_internal(self, cb, period):
+        # FIXME access to self._listeners is not thread-safe; this could be an
+        # issue when multiple WebUIs are being accessed for the same fuzzer.
+        ctx = self._create_event_context()
+        while not profiler.ProfilingStoppedEvent.is_set():
+            async with ctx:
+                if self._args is None:
+                    error("Event triggered while args is None. Check for data races in code!")
+                    continue
+                current_task().last_visit = now()
+                await cb(*self._args[0], **self._args[1], ret=self._ret)
+
+            # ensure that we don't sleep needlessly between periods
+            time_elapsed = now() - current_task().last_visit
+            if period is not None and time_elapsed < period:
+                await asyncio.sleep(period - time_elapsed)
+
+    def listener(self, period=None):
+        return partial(self._listener_internal, period=period)
+
+class EventContextManager:
+    def __init__(self, event_profiler):
+        self._prof = event_profiler
+        self._event = asyncio.Event()
+        self.loop = get_running_loop()
+        contexts = self._prof._listeners.get(self.loop, set())
+        contexts.add(self)
+        self._prof._listeners[self.loop] = contexts
+
     async def __aenter__(self):
-        await self._listener_event.wait()
-        return self
+        await self._event.wait()
 
     async def __aexit__(self, exc_type, exc_value, exc_traceback):
-        self._args = None
-        self._ret = None
-        self._listener_event.clear()
+        # consume the event at exit
+        self._prof._args = None
+        self._prof._ret = None
+        self._event.clear()
 
-    @property
-    def _listener_event(self):
-        return get_running_loop().events[id(self)]
-
-    @_listener_event.setter
-    def _listener_event(self, event):
-        get_running_loop().events[id(self)] = event
-
-    async def _listener_notify(self, args, ret):
+    async def notify(self, args, ret):
         # Since this is a coroutine, it could be scheduled at an arbitrary point
         # in time and could thus result in setting the event long after the args
         # had been consumed by the original function. To ensure that the args
@@ -80,31 +105,11 @@ class ProfileEvent(ProfilerBase):
         # * self.__aexit()__ clears args
         # * this coroutine is executed and the event is set
         # * self.__aenter()__ consumes this event, but args are None
-        self._args = args
-        self._ret = ret
-        self._listener_event.set()
+        self._prof._args = args
+        self._prof._ret = ret
+        self._event.set()
 
-    async def _listener_internal(self, cb, period):
-        # FIXME access to self._listeners is not thread-safe; this could be an
-        # issue when multiple WebUIs are being accessed for the same fuzzer.
-        try:
-            self._listeners.append(get_running_loop())
-            self._listener_event = asyncio.Event()
-            while not profiler.ProfilingStoppedEvent.is_set():
-                async with self:
-                    if self._args is None:
-                        error("Event triggered while args is None. Check for data races in code!")
-                        continue
-                    current_task().last_visit = now()
-                    await cb(*self._args[0], **self._args[1], ret=self._ret)
-
-                # ensure that we don't sleep needlessly between periods
-                time_elapsed = now() - current_task().last_visit
-                if period is not None and time_elapsed < period:
-                    await asyncio.sleep(period - time_elapsed)
-        finally:
-            del get_running_loop().events[id(self)]
-            self._listeners.remove(get_running_loop())
-
-    def listener(self, period=None):
-        return partial(self._listener_internal, period=period)
+    def __del__(self):
+        if self._event is not None:
+            self._prof._listeners.discard(self)
+            self._event = None
