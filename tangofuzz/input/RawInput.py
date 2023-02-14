49661345295c23c5cf64486b6ed5ed87cc2242ef
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2023-02-14 20:27:36 +0100

    RawInput transmission chunk size is adjustable

--- tangofuzz/input/RawInput.py
+++ tangofuzz/input/RawInput.py
@@ -1,13 +1,19 @@
-from input       import SerializedMetaInput 
+from input       import SerializedMetaInput
 from typing      import Iterable
 from interaction import InteractionBase, TransmitInteraction
 import struct
 
 class RawInput(metaclass=SerializedMetaInput, typ='raw'):
+    CHUNKSIZE = 4
+
     def loadi(self) -> Iterable[InteractionBase]:
         data = self._file.read()
-        for c in struct.unpack(f'{len(data)}c', data):
-            interaction = TransmitInteraction(data=c)
+        unpack_len = len(data) - (len(data) % self.CHUNKSIZE)
+        for s, in struct.iter_unpack(f'{self.CHUNKSIZE}s', data[:unpack_len]):
+            interaction = TransmitInteraction(data=s)
+            yield interaction
+        if unpack_len < len(data):
+            interaction = TransmitInteraction(data=data[unpack_len:])
             yield interaction
 
     def dumpi(self, itr: Iterable[InteractionBase], /):
