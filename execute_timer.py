7194bbe1d396496b549a253320432dcd638cae8b
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2023-02-15 19:52:26 +0100

    Added timing profiler for channel throughput

--- execute_timer.py
+++ execute_timer.py
@@ -1,24 +1,39 @@
 from tangofuzz.fuzzer import FuzzerConfig
 from tangofuzz.common import timeit
+
 import asyncio
 import argparse
 import logging
 from subprocess import PIPE
+from functools import wraps
 
 def parse_args():
-    parser = argparse.ArgumentParser(description=(
-        "Tests the reset rate of a target."
+    top = argparse.ArgumentParser(description=(
+        "Measures the execution rates of different fuzzer operations."
     ))
-    parser.add_argument("config",
+    top.add_argument('-C', '--config', required=True,
         help="The path to the TangoFuzz fuzz.json file.")
-    parser.add_argument('-S', "--samples", default=1000, type=int,
+    top.add_argument('-S', '--samples', default=1000, type=int,
         help="The number of samples to measure.")
-    parser.add_argument('--override', action='append', nargs=2)
-    parser.add_argument('-v', '--verbose', action='count', default=0,
+    top.add_argument('--override', action='append', nargs=2)
+    top.add_argument('-v', '--verbose', action='count', default=0,
         help=("Controls the verbosity of messages. "
             "-v prints info. -vv prints debug. Default: warnings and higher.")
         )
-    return parser.parse_args()
+    sub_top = top.add_subparsers(dest='command')
+
+    # loader reset rate
+    reset = sub_top.add_parser('reset', description=(
+        "Restart target to root state"
+    ))
+
+    # channel throughput
+    throughput = sub_top.add_parser('throughput', description=(
+        "Start target and send input file contents over the channel"
+    ))
+    throughput.add_argument('file', help="The path to the input file.")
+
+    return top.parse_args()
 
 def configure_verbosity(level):
     mapping = {
@@ -30,15 +45,26 @@ def configure_verbosity(level):
     numeric_level = mapping[level]
     logging.getLogger().setLevel(numeric_level)
 
-async def measure_time(config, samples):
-    # load at least once without measuring
+async def measure_load_time(config, args):
+    # load once without measuring
     ld = await config.loader
     await ld.load_state(None, None)
 
     async def load_many():
-        for _ in range(samples):
+        for _ in range(args.samples):
             await ld.load_state(None, None)
     await timeit(load_many)()
+
+async def measure_send_time(config, args):
+    gen = await config.input_generator
+    inp = gen.load_input(args.file)
+    ld = await config.loader
+    await ld.load_state(None, None)
+
+    async def do_many():
+        for _ in range(args.samples):
+            await ld.execute_input(inp)
+    await timeit(do_many)()
 
 def main():
     args = parse_args()
@@ -64,7 +90,13 @@ def main():
     config = FuzzerConfig(args.config, overrides)
     config._config["exec"].pop("stdout", None)
     config._config["exec"].pop("stderr", None)
-    asyncio.run(measure_time(config, args.samples))
+
+    command = {
+        'reset': measure_load_time,
+        'throughput': measure_send_time
+    }[args.command]
+
+    asyncio.run(command(config, args))
 
 if __name__ == '__main__':
     main()
