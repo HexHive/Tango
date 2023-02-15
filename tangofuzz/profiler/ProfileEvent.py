e480777f8875cd5948ae1a94d56fd80513c04874
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2023-02-15 11:32:47 +0100

    Fixed concurrency bug in ProfileEvent and improved performance

--- tangofuzz/profiler/ProfileEvent.py
+++ tangofuzz/profiler/ProfileEvent.py
@@ -4,8 +4,7 @@ import profiler
 from profiler import ProfilerBase
 from functools import partial
 import asyncio
-from asyncio import (get_running_loop, run_coroutine_threadsafe, wait_for,
-                    current_task)
+from asyncio import get_running_loop, run_coroutine_threadsafe
 from time import perf_counter as now
 from asyncio import iscoroutinefunction
 
@@ -19,7 +18,8 @@ class ProfileEvent(ProfilerBase):
     def _notify_listeners(self, argt, ret):
         for loop, contexts in self._listeners.items():
             for context in contexts:
-                run_coroutine_threadsafe(context.notify(argt, ret), loop)
+                if not context.pending:
+                    run_coroutine_threadsafe(context.notify(argt, ret), loop)
 
     def ___call___(self, obj):
         def func(*args, **kwargs):
@@ -42,36 +42,35 @@ class ProfileEvent(ProfilerBase):
     def value(self):
         raise NotImplementedError()
 
-    @property
-    def args(self):
-        return self._args
-
-    @property
-    def ret(self):
-        return self._ret
-
     def _create_event_context(self):
-        return EventContextManager(self)
+        ctx = EventContextManager(self)
+        loop = get_running_loop()
+        if (listeners := self._listeners.get(loop)) is not None:
+            listeners.add(ctx)
+        else:
+            self._listeners[loop] = {ctx}
+        return ctx
 
     async def _listener_internal(self, cb, period):
         # FIXME access to self._listeners is not thread-safe; this could be an
         # issue when multiple WebUIs are being accessed for the same fuzzer.
         ctx = self._create_event_context()
+        last_visit = now()
         try:
-            while not profiler.ProfilingStoppedEvent.is_set():
+            while True:
                 async with ctx as profiling_stopped:
                     if profiling_stopped:
-                        continue
-                    if self._args is None:
+                        return
+                    if ctx.args is None:
                         error("Event triggered while args is None. Check for data races in code!")
                         continue
-                    last_visit = now()
-                    await cb(*self._args[0], **self._args[1], ret=self._ret)
+                    await cb(*ctx.args[0], **ctx.args[1], ret=ctx.ret)
 
-                # ensure that we don't sleep needlessly between periods
-                time_elapsed = now() - last_visit
-                if period is not None and time_elapsed < period:
-                    await asyncio.sleep(period - time_elapsed)
+                    # ensure that we don't sleep needlessly between periods
+                    time_elapsed = now() - last_visit
+                    if period is not None and time_elapsed < period:
+                        await asyncio.sleep(period - time_elapsed)
+                last_visit = now()
         except asyncio.CancelledError:
             return
 
@@ -82,15 +81,11 @@ class EventContextManager:
     def __init__(self, event_profiler):
         self._prof = event_profiler
         self._event = asyncio.Event()
-        self.loop = get_running_loop()
-        contexts = self._prof._listeners.get(self.loop, set())
-        contexts.add(self)
-        self._prof._listeners[self.loop] = contexts
         self._stop_watcher = asyncio.ensure_future(profiler.ProfilingStoppedEvent.wait())
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> bool:
         event_notify = asyncio.ensure_future(self._event.wait())
-        done, pending = await asyncio.wait((self._stop_watcher, event_notify),
+        done, _ = await asyncio.wait((self._stop_watcher, event_notify),
             return_when=asyncio.FIRST_COMPLETED)
         if self._stop_watcher in done:
             event_notify.cancel()
@@ -100,8 +95,8 @@ class EventContextManager:
 
     async def __aexit__(self, exc_type, exc_value, exc_traceback):
         # consume the event at exit
-        self._prof._args = None
-        self._prof._ret = None
+        self._args = None
+        self._ret = None
         self._event.clear()
 
     async def notify(self, args, ret):
@@ -118,12 +113,21 @@ class EventContextManager:
         # * self.__aexit()__ clears args
         # * this coroutine is executed and the event is set
         # * self.__aenter()__ consumes this event, but args are None
-        self._prof._args = args
-        self._prof._ret = ret
+        self._args = args
+        self._ret = ret
         self._event.set()
 
+    @property
+    def pending(self) -> bool:
+        return self._event.is_set()
+
+    @property
+    def args(self):
+        return self._args
+
+    @property
+    def ret(self):
+        return self._ret
+
     def __del__(self):
-        if self._event is not None:
-            self._prof._listeners.discard(self)
-            self._event = None
         self._stop_watcher.cancel()
