204e459a988d19ceb9a72dde53ff09931057a6b8
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2023-03-02 10:48:31 +0100

    Fixed hash32 function not incorporating trailing bytes

--- lib/coverage.c
+++ lib/coverage.c
@@ -76,11 +76,13 @@ static inline u32 hash32(const void* key, u32 len, u32 seed)
   const u64* data = (u64*)key;
   u64 h1 = seed ^ len;
 
+  rem = len & 0b111;
   len >>= 3;
 
   while (len--) {
 
     u64 k1 = *data++;
+    key += 1<<3;
 
     k1 *= 0x87c37b91114253d5ULL;
     k1  = ROL64(k1, 31);
@@ -91,6 +93,17 @@ static inline u32 hash32(const void* key, u32 len, u32 seed)
     h1  = h1 * 5 + 0x52dce729;
 
   }
+
+  u64 data_rem = 0;
+  for (int i = 0; i < rem; ++i)
+    data_rem += *((u8*)key++) << (8 * i);
+  u64 k1 = data_rem;
+  k1 *= 0x87c37b91114253d5ULL;
+  k1  = ROL64(k1, 31);
+  k1 *= 0x4cf5ad432745937fULL;
+  h1 ^= k1;
+  h1  = ROL64(h1, 27);
+  h1  = h1 * 5 + 0x52dce729;
 
   h1 ^= h1 >> 33;
   h1 *= 0xff51afd7ed558ccdULL;
