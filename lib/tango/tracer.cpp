994374df6f636c32b7cd3748e00bfac4d1375dd2
Ahmad Hazimeh <ahmad+gh@hazimeh.dev>
2023-04-06 00:25:12 +0200

    Added skeleton for cmp tracing

--- lib/tango/tracer.cpp
+++ lib/tango/tracer.cpp
@@ -100,6 +100,22 @@ inline void Tracer::HandleTracePCGuard(uintptr_t pc, uint32_t* guard) {
         feature_map[idx] = UINT8_MAX;
 }
 
+template <class T>
+ATTRIBUTE_NO_SANITIZE_ALL
+inline void Tracer::HandleCmp(uintptr_t PC, T Arg1, T Arg2) {
+    uint64_t ArgXor = Arg1 ^ Arg2;
+    switch (sizeof(T)) {
+    case 2:
+        TORC2.Insert(Arg1, Arg2);
+    case 4:
+        TORC4.Insert(Arg1, Arg2);
+    case 8:
+        TORC8.Insert(Arg1, Arg2);
+    default:
+        return;
+    }
+}
+
 } // namespace fuzzer
 
 
@@ -115,5 +131,100 @@ void __sanitizer_cov_trace_pc_guard(uint32_t *guard) {
     uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
     fuzzer::CoverageTracer.HandleTracePCGuard(PC, guard);
 }
+
+#ifdef USE_CMPLOG
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_cmp8(uint64_t Arg1, uint64_t Arg2) {
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  fuzzer::CoverageTracer.HandleCmp(PC, Arg1, Arg2);
+}
+
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_const_cmp8(uint64_t Arg1, uint64_t Arg2) {
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  fuzzer::CoverageTracer.HandleCmp(PC, Arg1, Arg2);
+}
+
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_cmp4(uint32_t Arg1, uint32_t Arg2) {
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  fuzzer::CoverageTracer.HandleCmp(PC, Arg1, Arg2);
+}
+
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_const_cmp4(uint32_t Arg1, uint32_t Arg2) {
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  fuzzer::CoverageTracer.HandleCmp(PC, Arg1, Arg2);
+}
+
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_cmp2(uint16_t Arg1, uint16_t Arg2) {
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  fuzzer::CoverageTracer.HandleCmp(PC, Arg1, Arg2);
+}
+
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_const_cmp2(uint16_t Arg1, uint16_t Arg2) {
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  fuzzer::CoverageTracer.HandleCmp(PC, Arg1, Arg2);
+}
+
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_cmp1(uint8_t Arg1, uint8_t Arg2) {
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  fuzzer::CoverageTracer.HandleCmp(PC, Arg1, Arg2);
+}
+
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_const_cmp1(uint8_t Arg1, uint8_t Arg2) {
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  fuzzer::CoverageTracer.HandleCmp(PC, Arg1, Arg2);
+}
+
+ATTRIBUTE_NO_SANITIZE_ALL
+void __sanitizer_cov_trace_switch(uint64_t Val, uint64_t *Cases) {
+  uint64_t N = Cases[0];
+  uint64_t ValSizeInBits = Cases[1];
+  uint64_t *Vals = Cases + 2;
+  // Skip the most common and the most boring case: all switch values are small.
+  // We may want to skip this at compile-time, but it will make the
+  // instrumentation less general.
+  if (Vals[N - 1]  < 256)
+    return;
+  // Also skip small inputs values, they won't give good signal.
+  if (Val < 256)
+    return;
+  uintptr_t PC = reinterpret_cast<uintptr_t>(GET_CALLER_PC());
+  size_t i;
+  uint64_t Smaller = 0;
+  uint64_t Larger = ~(uint64_t)0;
+  // Find two switch values such that Smaller < Val < Larger.
+  // Use 0 and 0xfff..f as the defaults.
+  for (i = 0; i < N; i++) {
+    if (Val < Vals[i]) {
+      Larger = Vals[i];
+      break;
+    }
+    if (Val > Vals[i]) Smaller = Vals[i];
+  }
+
+  // Apply HandleCmp to {Val,Smaller} and {Val, Larger},
+  // use i as the PC modifier for HandleCmp.
+  if (ValSizeInBits == 16) {
+    fuzzer::CoverageTracer.HandleCmp(PC + 2 * i, static_cast<uint16_t>(Val),
+                          (uint16_t)(Smaller));
+    fuzzer::CoverageTracer.HandleCmp(PC + 2 * i + 1, static_cast<uint16_t>(Val),
+                          (uint16_t)(Larger));
+  } else if (ValSizeInBits == 32) {
+    fuzzer::CoverageTracer.HandleCmp(PC + 2 * i, static_cast<uint32_t>(Val),
+                          (uint32_t)(Smaller));
+    fuzzer::CoverageTracer.HandleCmp(PC + 2 * i + 1, static_cast<uint32_t>(Val),
+                          (uint32_t)(Larger));
+  } else {
+    fuzzer::CoverageTracer.HandleCmp(PC + 2*i, Val, Smaller);
+    fuzzer::CoverageTracer.HandleCmp(PC + 2*i + 1, Val, Larger);
+  }
+}
+#endif
 
 } // extern "C"
--- lib/tango/tracer.h
+++ lib/tango/tracer.h
@@ -5,6 +5,31 @@
 
 namespace fuzzer {
 
+template<class T, size_t kCapacityT>
+struct TableOfRecentCompares {
+    static const size_t kCapacity = kCapacityT;
+    size_t Length = 0;
+    size_t LastIdx = 0;
+
+    struct Pair {
+        T A, B;
+    };
+
+    void Insert(const T &Arg1, const T &Arg2) {
+        Table[LastIdx].A = Arg1;
+        Table[LastIdx].B = Arg2;
+        LastIdx = (LastIdx + 1) % kCapacity;
+        if (Length < kCapacity)
+            ++Length;
+    }
+
+    void Clear() {
+        Length = LastIdx = 0;
+    }
+
+    Pair Table[kCapacity];
+};
+
 class Tracer {
 public:
     Tracer();
@@ -13,12 +38,18 @@ public:
     void ClearMaps();
     void InitializeGuards(uint32_t *start, uint32_t *stop);
     void HandleTracePCGuard(uintptr_t pc, uint32_t* guard);
+    template <class T> void HandleCmp(uintptr_t PC, T Arg1, T Arg2);
 
 private:
     bool initialized;
     bool disabled;
     size_t num_guards;
     uint8_t *feature_map;
+
+    TableOfRecentCompares<uint8_t, 1024> TORC1;
+    TableOfRecentCompares<uint16_t, 1024> TORC2;
+    TableOfRecentCompares<uint32_t, 1024> TORC4;
+    TableOfRecentCompares<uint64_t, 1024> TORC8;
 };
 
 extern Tracer CoverageTracer;
