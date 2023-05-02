0af7e462216d648641c8926f0015f62910ebed89
Ahmad Hazimeh <ahmad.hazimeh@epfl.ch>
2023-05-02 15:46:25 +0200

    Fixed issue with capabilities

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ USER=${USER:-$(whoami)}
 ARGS="$@"
 
 caps=(cap_net_admin cap_sys_admin cap_dac_override cap_chown cap_fowner \
-	  cap_setpcap)
+	  cap_setpcap cap_setuid cap_setgid)
 IFS=','
 caps="${caps[*]}"=eip
 unset IFS
 
-sudo -E capsh --user=$USER --keep=1 --caps=$caps -- \
+sudo -E capsh --caps=$caps --user=$USER --addamb=$caps -- \
 	-c "$PYBIN $PYSCRIPT $ARGS"
