1effb2375f8db2a7ba00b20a009c2613f137c8d6
Qiang Liu <cyruscyliu@gmail.com>
2023-12-25 21:53:52 +0100

    update fuzz.json

--- scripts_eurosp/docker_entry.sh
+++ scripts_eurosp/docker_entry.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 cd $HOME/tango
+./scripts_eurosp/git_install.sh -y
+sudo ln -s /usr/bin/clang-13 /usr/bin/clang
+sudo ln -s /usr/bin/clang++-13 /usr/bin/clang++
 source .venv/bin/activate
 
 python="$(realpath "$(which python)")"
-sudo setcap cap_net_admin,cap_sys_admin,cap_dac_override+eip $python
+sudo setcap \
+    cap_net_admin,cap_sys_admin,cap_dac_override,cap_chown,cap_fowner,cap_setpcap,cap_setuid,cap_setgid+eip \
+    $python
 
 target=$1
 shift
