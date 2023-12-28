2900134ccfa667edfbbfae3274fc8fb2e3ebfea5
hoodiney <845928861@qq.com>
2023-12-28 17:15:59 +0100

    support files for daap, not finished

---
+++ targets/daap/preinstall.sh
@@ -0,0 +1,13 @@
+sudo apt-get -y update && 
+sudo apt install -y \
+    apt-utils \
+    libgnutls28-dev \
+    net-tools \
+    bison \
+    flex \
+    autotools-dev autoconf automake libtool gettext gawk \
+    gperf antlr3 libantlr3c-dev libconfuse-dev libunistring-dev libsqlite3-dev \
+    libavcodec-dev libavformat-dev libavfilter-dev libswscale-dev libavutil-dev \
+    libasound2-dev libmxml-dev libgcrypt20-dev libavahi-client-dev zlib1g-dev \
+    libevent-dev libplist-dev libsodium-dev libjson-c-dev libwebsockets-dev \
+    libcurl4-openssl-dev avahi-daemon
