5ed3115f3fa388ca9b9c908b47b7ffbdb5ad4867
Qiang Liu <cyruscyliu@gmail.com>
2024-01-25 17:11:47 +0100

    Add cap_sys_ptrace to run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ USER=${USER:-$(whoami)}
 ARGS="$@"
 
 caps=(cap_net_admin cap_sys_admin cap_dac_override cap_chown cap_fowner \
-	  cap_setpcap cap_setuid cap_setgid)
+	  cap_setpcap cap_setuid cap_setgid cap_sys_ptrace)
 IFS=','
 caps="${caps[*]}"=eip
 unset IFS
