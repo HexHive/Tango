HexHive/Tango

commit 37f0e4682e02402df305664fb64e3ad1599169dc

adjust PCAPInput for the ftp PORT command

tango/net.py
@@ -951,6 +951,11 @@ class PCAPInput(metaclass=SerializedInputMeta, typ='pcap'):
             for endpoint in eps:
                 if endpoint not in endpoints:
                     endpoints.append(endpoint)
+        if self._fmt.protocol == 'tcp':
+            for i in range(len(plist)):
+                if plist[i][Raw].load.decode('utf-8').startswith("PORT"):
+                    plist[i][Raw].load = b"PORT 127,0,0,1,222,193\r\n"
+
         if len(endpoints) != 2:
             # raise RuntimeError(
             #     f"PCAP file has {len(endpoints)} endpoints (expected 2)"

commit ce34535b9ad80cc88fd34fa808147930390a86f3

Fix a bug introduced by 37f0e46

tango/net.py
@@ -953,8 +953,12 @@ class PCAPInput(metaclass=SerializedInputMeta, typ='pcap'):
                     endpoints.append(endpoint)
         if self._fmt.protocol == 'tcp':
             for i in range(len(plist)):
-                if plist[i][Raw].load.decode('utf-8').startswith("PORT"):
-                    plist[i][Raw].load = b"PORT 127,0,0,1,222,193\r\n"
+                try:
+                    if plist[i][Raw].load.startswith(b"PORT"):
+                        plist[i][Raw].load = b"PORT 127,0,0,1,222,193\r\n"
+                except IndexError:
+                    # Raw doesn't exit
+                    pass
 
         if len(endpoints) != 2:
             # raise RuntimeError(